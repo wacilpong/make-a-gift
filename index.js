6a2ac4b52d5468a48c02b182f41b02d4e06d2aac
Roomy <cilpong@gmail.com>
2025-03-29 15:59:18 +0900

    fix: vite에서는 js연결시 type="module"로 해야댐

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ function download() {
   alert("다운받기");
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+function display(form, event) {
+  event.preventDefault();
+
   const congrats = document.getElementById("congrats_box");
+  const data = new FormData(form);
+  const person = data.get('person');
+  const gift = data.get('gift');
+
+  alert(`${person}에게 ${gift}을 선물로 드릴게요.`);
+  congrats.style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("birthday_form");
+  const button = document.getElementById("download_button");
 
-  form.onsubmit = (event) => {
-    event.preventDefault();
-
-    const data = new FormData(form);
-    const person = data.get('person');
-    const gift = data.get('gift');
-
-    alert(`${person}에게 ${gift}을 선물로 드릴게요.`);
-    congrats.style.display = 'block';
-  }
+  button.onclick = () => download();
+  form.onsubmit = (event) => display(form, event)
 });
